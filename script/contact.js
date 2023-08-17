bd7bc4f99748dba64cc9f4a5a079d9dc224a0db0
Akash822m <akash822m@gmail.com>
2023-08-17 02:02:45 -0400

    improved responsiveness

--- script/contact.js
+++ script/contact.js
@@ -4,9 +4,6 @@
 
     contactForm.addEventListener("submit", function (event) {
       event.preventDefault();
-      // Here you can add code to send the form data to your server
-      // using XMLHttpRequest or Fetch API.
-      // For this example, we'll just show the "Thank you" message.
       thankYouMessage.style.display = "block";
     });
   });
